cea8a3a964a4e85f0dad3b8ea2ba0c4e613c3dd4
Dzaky <dzaky1187@gmail.com>
2024-10-28 14:14:33 +0700

    TERBARU

---
+++ belajar_subprogram.py
@@ -0,0 +1,6 @@
+def mencari_luas_lingkaran(jari-jari):
+    luas_lingkaran = 3.14 *jari-jari*jari-jari
+    return luas_lingkaran 
+
+lingkaran_pertama=mencari_luas_lingkaran(S)
+print(lingkaran_pertama)
